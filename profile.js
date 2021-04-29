80775b682a4fac09f95286b0b18a5f1008d732ee
ivanruedas85 <ivanruedas85@gmail.com>
2021-04-29 22:48:36 +0000

    agregando profile.js

---
+++ profile.js
@@ -0,0 +1,157 @@
+export const skills = [
+  {
+    skill: "Javascript",
+    percentage: 80,
+  },
+  {
+    skill: "Golang",
+    percentage: 40,
+  },
+  {
+    skill: "Nodejs",
+    percentage: 90,
+  },
+  {
+    skill: "Photoshop",
+    percentage: 90,
+  },
+  {
+    skill: "Adobe Lightroom",
+    percentage: 90,
+  },
+  {
+    skill: "Python",
+    percentage: 90,
+  },
+  {
+    skill: "Html",
+    percentage: 90,
+  },
+  {
+    skill: "Css",
+    percentage: 70,
+  },
+  {
+    skill: "Sass",
+    percentage: 80,
+  },
+  {
+    skill: "React.js",
+    percentage: 90,
+  },
+  {
+    skill: "Next.js",
+    percentage: 40,
+  },
+  {
+    skill: "Arduino",
+    percentage: 80,
+  },
+  {
+    skill: "Docker",
+    percentage: 50,
+  },
+];
+
+export const experiences = [
+  {
+    title: "Sistema de rastreo de vulnerabilidades",
+    description:
+      "Cree aplicaciones automatizadas usando python para escaneo de vulnerabilidades en redes.",
+    from: 2015,
+    to: 2016,
+  },
+  {
+    title: "Reconstrucción de fotografía.",
+    description:
+      "Eliminado de manchas, creación de tonos de colores, texturas y formas en fotografías dañadas usando Adobe Photoshop.",
+    from: 2017,
+  },
+  {
+    title: "Sistema de riego automático",
+    description:
+      "Diseño de esquemas, ensamblado de partes y programación en Arduino de sistema esterilización de agua y riego automático para huertos urbanos.",
+    from: 2016,
+    to: 2018,
+  },
+  {
+    title: "Diseño y fabricación de muebles.",
+    description:
+      "Diseño y fabricación de muebles minimalistas y personalizados de madera tryplay y o laminas de acrílico para el hogar u oficina.",
+    from: 2019,
+  },
+  {
+    title: "Restructuración y mantenimiento de red.",
+    description:
+      "Acondicionamiento de red del hogar, usando nuevas tecnologías de administración de paqueterías y bases de datos, ampliación de espacio de almacenamiento en disco, ampliación de memoria ram, ampliación de GPU's, actualización de tarjeta madre y procesador, diseño de espacio de alojamiento del servidor, diseño de estructura de conexión a cada rincón del hogar.",
+    from: 2020,
+  },
+];
+
+export const projects = [
+  {
+    name: "Boton pegajoso",
+    description:
+      "Efecto esta echo con Html, JavaScript y la libreria lib-gooey, CSS",
+    image: "Boton-Pegajoso.png",
+    url: "https://gitlab.com/ivanruedas85/botonefectopegajoso"
+  },
+  {
+    name: "Carrousel 3d con html y css",
+    description:
+      "Carrusel 3d de imagenes, usando solo html, css y Js",
+    image: "Carrousel.png",
+    url: "https://gitlab.com/ivanruedas85/carrousel-3d"
+  },
+  {
+    name: "Efecto Parallax",
+    description:
+      "Efecto realizado con html y css",
+    image: "Efectoparalax.png",
+    url: "https://gitlab.com/ivanruedas85/efectoparalax"
+  },
+  {
+    name: "Filter Image",
+    description:
+      "Galería de fotos con filtro de acercamiento, usando html, css, js, y jquery.",
+    image: "Filter-Images.png",
+    url: "https://gitlab.com/ivanruedas85/filter-images"
+  },
+  {
+    name: "Encabezado Responsive",
+    description:
+      "Encabezado responsivo solo con css.",
+    image: "EncabezadoResponsive.png",
+    url: "https://gitlab.com/ivanruedas85/etiquetaencabezadoresponsive"
+  },
+  {
+    name: "Awesome Website 6",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "/",
+  },
+];
+
+export const posts = [
+  {
+    title: "React",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Angular",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Nextjs",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+];
